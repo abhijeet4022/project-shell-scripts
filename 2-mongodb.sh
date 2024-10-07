345e83e9f9aa560e045cd6b4607eca73eea498ff
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-07 14:40:26 +0530

    Project : MongoDB Component

--- 2-mongodb.sh
+++ 2-mongodb.sh
@@ -2,15 +2,15 @@ component=mongodb
 log="/tmp/$component.log"
 echo -e "\e[1;36m--- MongoDB Database Setup ---\e[0m" | tee -a $log
 
-echo -e "\e[32mConfiguring the MongoDB repository\e[0m" | tee -a $log
+echo -e "\e[32mConfiguring the MongoDB repository.\e[0m" | tee -a $log
 cp mongodb-org-4.2.repo /etc/yum.repos.d/mongodb-org-4.2.repo &>> $log
 
-echo -e "\e[32mInstalling MongoDB\e[0m" | tee -a $log
+echo -e "\e[32mInstalling MongoDB.\e[0m" | tee -a $log
 dnf install mongodb-org -y &>> $log
 
-echo -e "\e[32mAllowing external access to MongoDB\e[0m" | tee -a $log
+echo -e "\e[32mAllowing external access to MongoDB.\e[0m" | tee -a $log
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $log
 
-echo -e "\e[32mRestarting and enabling the MongoDB service\e[0m" | tee -a $log
+echo -e "\e[32mRestarting and enabling the MongoDB service.\e[0m" | tee -a $log
 systemctl enable mongod &>> $log
 systemctl restart mongod &>> $log
