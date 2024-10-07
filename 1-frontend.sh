a2aaa64079cd7f29d5791b45f6ff71d2eb24e7a5
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-07 06:22:32 +0530

    Project - Frontend Code

--- 1-frontend.sh
+++ 1-frontend.sh
@@ -2,6 +2,6 @@ dnf install nginx -y
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip -o /tmp/frontend.zip -d /usr/share/nginx/html/
-cp reverse-proxy.conf /etc/nginx/default.d/reverse-proxy.conf
+cp 1-reverse-proxy.conf /etc/nginx/default.d/reverse-proxy.conf
 systemctl restart nginx
 systemctl enable nginx
