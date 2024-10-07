76642b61c8977a1aacdf81f334e71762056c62da
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-07 14:34:29 +0530

    Project : Catalogue Component

--- 3-catalogue.sh
+++ 3-catalogue.sh
@@ -4,42 +4,42 @@ log="/tmp/$component.log"
 echo -e "\e[1;36m--- Catalogue Application Setup ---\e[0m" | tee -a $log
 
 echo -e "\e[32mDisabling the current Node.js module\e[0m" | tee -a $log
-dnf module disable nodejs -y &> $log
+dnf module disable nodejs -y &>> $log
 
 echo -e "\e[32mEnabling the Node.js 18 module\e[0m" | tee -a $log
-dnf module enable nodejs:18 -y &> $log
+dnf module enable nodejs:18 -y &>> $log
 
 echo -e "\e[32mInstalling Node.js\e[0m" | tee -a $log
-dnf install nodejs -y &> $log
+dnf install nodejs -y &>> $log
 
 echo -e "\e[32mDownloading the Catalogue application content code\e[0m" | tee -a $log
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &> $log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> $log
 
 echo -e "\e[32mCreating the application directory\e[0m" | tee -a $log
-mkdir /app &> $log
+mkdir /app &>> $log
 
 echo -e "\e[32mExtracting the application code\e[0m" | tee -a $log
-unzip -o /tmp/catalogue -d /app &> $log
+unzip -o /tmp/catalogue -d /app &>> $log
 
 echo -e "\e[32mDownloading application dependencies from package.json\e[0m" | tee -a $log
-npm install --prefix /app &> $log
+npm install --prefix /app &>> $log
 
 echo -e "\e[32mCreating the application user\e[0m" | tee -a $log
-useradd roboshop -c "Application User" &> $log
+useradd roboshop -c "Application User" &>> $log
 
 echo -e "\e[32mCreating the application service file\e[0m" | tee -a $log
-cp catalogue.service /etc/systemd/system/catalogue.service &> $log
+cp catalogue.service /etc/systemd/system/catalogue.service &>> $log
 
 echo -e "\e[32mEnabling and restarting the application service\e[0m" | tee -a $log
-systemctl daemon-reload &> $log
-systemctl restart catalogue.service &> $log
-systemctl enable catalogue.service &> $log
+systemctl daemon-reload &>> $log
+systemctl restart catalogue.service &>> $log
+systemctl enable catalogue.service &>> $log
 
 echo -e "\e[32mConfiguring the MongoDB repository\e[0m" | tee -a $log
-cp mongodb-org-4.2.repo /etc/yum.repos.d/mongodb-org-4.2.repo &> $log
+cp mongodb-org-4.2.repo /etc/yum.repos.d/mongodb-org-4.2.repo &>> $log
 
 echo -e "\e[32mInstalling MongoDB client to load the schema\e[0m" | tee -a $log
-dnf install mongodb-org-shell -y &> $log
+dnf install mongodb-org-shell -y &>> $log
 
 echo -e "\e[32mLoading the schema for the application\e[0m" | tee -a $log
-mongo --host mongodb.learntechnology.cloud </app/schema/catalogue.js &> $log
+mongo --host mongodb.learntechnology.cloud </app/schema/catalogue.js &>> $log
