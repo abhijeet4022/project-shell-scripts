fb52a9845cba779960ffb2c858f94bf07c7a33b2
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-07 06:34:26 +0530

    Project - MongoDB Code

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 cp mongodb-org-4.2.repo /etc/yum.repos.d/mongodb-org-4.2.repo
 dnf install mongodb-org -y
-sed -i 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
