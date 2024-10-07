1ecd3ecc104e91044ebfbb04b0501e50dc93d9c4
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-07 09:46:04 +0530

    Project - Frontend Code

--- 1-frontend.sh
+++ 1-frontend.sh
@@ -1,20 +1,22 @@
-echo -e "\e[1;36m--- Nginx Web Server Setup ---\e[0m"
+component=frontend
+log="/tmp/$component.log"
+echo -e "\e[1;36m--- Nginx Web Server Setup ---\e[0m" | tee -a $log
 
-echo -e "\e[32mStep 1: Installing Nginx\e[0m"
+echo -e "\e[32mStep 1: Installing Nginx\e[0m" | tee -a $log
 dnf install nginx -y
 
-echo -e "\e[32mStep 2: Removing default Nginx content\e[0m"
+echo -e "\e[32mStep 2: Removing default Nginx content\e[0m" | tee -a $log
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[32mStep 3: Downloading the frontend web layer code\e[0m"
+echo -e "\e[32mStep 3: Downloading the frontend web layer code\e[0m" | tee -a $log
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[32mStep 4: Extracting the web layer code\e[0m"
+echo -e "\e[32mStep 4: Extracting the web layer code\e[0m" | tee -a $log
 unzip -o /tmp/frontend.zip -d /usr/share/nginx/html/
 
-echo -e "\e[32mStep 5: Configuring reverse proxy settings\e[0m"
+echo -e "\e[32mStep 5: Configuring reverse proxy settings\e[0m" | tee -a $log
 cp 1-reverse-proxy.conf /etc/nginx/default.d/reverse-proxy.conf
 
-echo -e "\e[32mStep 6: Restarting and enabling the Nginx service\e[0m"
+echo -e "\e[32mStep 6: Restarting and enabling the Nginx service\e[0m" | tee -a $log
 systemctl restart nginx
 systemctl enable nginx
