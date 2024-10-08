28bcaf5b7a701e097fdaf4aee46225958f93c64b
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-08 07:00:32 +0530

    Project : Frontend Component

--- 1-frontend.sh
+++ 1-frontend.sh
@@ -1,5 +1,6 @@
 component=frontend
 log="/tmp/$component.log"
+
 echo -e "\e[1;36m--- Nginx Web Server Setup ---\e[0m" | tee -a $log
 
 echo -e "\e[32mInstalling Nginx.\e[0m" | tee -a $log
@@ -18,5 +19,6 @@ echo -e "\e[32mConfiguring reverse proxy settings.\e[0m" | tee -a $log
 cp config-files/1-reverse-proxy.conf /etc/nginx/default.d/reverse-proxy.conf &>> $log 
 
 echo -e "\e[32mRestarting and enabling the Nginx service.\e[0m" | tee -a $log
+systemctl daemon-reload &>> $log
 systemctl restart nginx &>> $log 
 systemctl enable nginx &>> $log 
