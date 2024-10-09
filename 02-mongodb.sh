385b2960bad36de163c8fb709ce7eafc66241f96
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-09 17:36:48 +0530

    Project: MongoDB Setup

--- 02-mongodb.sh
+++ 02-mongodb.sh
@@ -1,21 +1,21 @@
 component=mongodb
 source ./component_setup.sh
 
-echo -e "\e[1;36m--- MongoDB Database Setup ---\e[0m" | tee -a $log
+echo -e "\e[1;32m--- MongoDB Database Setup ---\e[0m" | tee -a $log
 
-echo -e "\e[32mConfiguring the MongoDB repository.\e[0m" | tee -a $log
+echo -e "\e[36mConfiguring the MongoDB repository.\e[0m" | tee -a $log
 cp repository-files/mongodb-org-4.2.repo /etc/yum.repos.d/mongodb-org-4.2.repo &>> $log
 func_exit_status
 
-echo -e "\e[32mInstalling MongoDB.\e[0m" | tee -a $log
+echo -e "\e[36mInstalling MongoDB.\e[0m" | tee -a $log
 dnf install mongodb-org -y &>> $log
 func_exit_status
 
-echo -e "\e[32mAllowing external access to MongoDB.\e[0m" | tee -a $log
+echo -e "\e[36mAllowing external access to MongoDB.\e[0m" | tee -a $log
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $log
 func_exit_status
 
-echo -e "\e[32mRestarting and enabling the MongoDB service.\e[0m" | tee -a $log
+echo -e "\e[36mRestarting and enabling the MongoDB service.\e[0m" | tee -a $log
 systemctl enable mongod &>> $log
 systemctl restart mongod &>>
 func_exit_status
