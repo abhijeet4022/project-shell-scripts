e6ac46d352e19cf93fb786c9ab3c7fd9a2db64f4
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-09 17:20:50 +0530

    Project: Shipping Setup

--- 08-shipping.sh
+++ 08-shipping.sh
@@ -1,6 +1,7 @@
 component="shipping"
 schema_type="mysql"
 source ./component_setup.sh
+
 mysql_root_password=$1
 if [ -z "${mysql_root_password}" ]; then
   echo "Root Password Missing: Please Pass the MySQL Root Password as First Argument"
