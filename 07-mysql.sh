fb2a92f90daf246c50c126106694a90b119771c0
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-09 17:37:06 +0530

    Project: MySQL Setup

--- 07-mysql.sh
+++ 07-mysql.sh
@@ -7,25 +7,25 @@ if [ -z "${mysql_root_password}" ]; then
   exit 1
 fi
 
-echo -e "\e[1;36m--- ${component} Application Setup ---\e[0m" | tee -a $log
+echo -e "\e[1;32m--- ${component} Application Setup ---\e[0m" | tee -a $log
 
-echo -e "\e[32mDisabling the current MySQL module.\e[0m" | tee -a $log
+echo -e "\e[36mDisabling the current MySQL module.\e[0m" | tee -a $log
 dnf module disable mysql -y &>> $log
 func_exit_status
 
-echo -e "\e[32mConfiguring the ${component} repository.\e[0m" | tee -a $log
+echo -e "\e[36mConfiguring the ${component} repository.\e[0m" | tee -a $log
 cp repository-files/mysql.repo /etc/yum.repos.d/mysql.repo &>> $log
 func_exit_status
 
-echo -e "\e[32mInstalling MySQL Community Server.\e[0m" | tee -a $log
+echo -e "\e[36mInstalling MySQL Community Server.\e[0m" | tee -a $log
 dnf install mysql-community-server -y &>> $log
 func_exit_status
 
-echo -e "\e[32mEnabling and starting the mysqld service.\e[0m" | tee -a $log
+echo -e "\e[36mEnabling and starting the mysqld service.\e[0m" | tee -a $log
 systemctl enable mysqld &>> $log
 systemctl restart mysqld &>> $log
 func_exit_status
 
-echo -e "\e[32mResetting the MySQL root password.\e[0m" | tee -a $log
+echo -e "\e[36mResetting the MySQL root password.\e[0m" | tee -a $log
 mysql_secure_installation --set-root-pass ${mysql_root_password} &>> $log
 func_exit_status
