902ce73f0ecc9139480d0fc0d9f561976e982527
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-09 17:36:57 +0530

    Project: Redis Setup

--- 04-redis.sh
+++ 04-redis.sh
@@ -1,25 +1,25 @@
 component=redis
 source ./component_setup.sh
 
-echo -e "\e[1;36m--- Redis Application Setup ---.\e[0m" | tee -a $log
+echo -e "\e[1;32m--- Redis Application Setup ---.\e[0m" | tee -a $log
 
-echo -e "\e[32mInstalling Remi repository for Redis.\e[0m" | tee -a $log
+echo -e "\e[36mInstalling Remi repository for Redis.\e[0m" | tee -a $log
 dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> $log
 func_exit_status
 
-echo -e "\e[32mEnabling the Redis 6.2 module from Remi repository.\e[0m" | tee -a $log
+echo -e "\e[36mEnabling the Redis 6.2 module from Remi repository.\e[0m" | tee -a $log
 dnf module enable redis:remi-6.2 -y &>> $log
 func_exit_status
 
-echo -e "\e[32mInstalling Redis.\e[0m" | tee -a $log
+echo -e "\e[36mInstalling Redis.\e[0m" | tee -a $log
 dnf install redis -y &>> $log
 func_exit_status
 
-echo -e "\e[32mConfiguring Redis to allow external access.\e[0m" | tee -a $log
+echo -e "\e[36mConfiguring Redis to allow external access.\e[0m" | tee -a $log
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>> $log
 func_exit_status
 
-echo -e "\e[32mEnabling and restarting Redis service.\e[0m" | tee -a $log
+echo -e "\e[36mEnabling and restarting Redis service.\e[0m" | tee -a $log
 systemctl enable redis &>> $log
 systemctl restart redis &>> $log
 func_exit_status
