3c12e2be2868158fe8b1cfdbd0bb2ecfdc1c8233
Abhijeet Pramanik <abhijeet4022@gmail.com>
2024-10-09 17:37:25 +0530

    Project: Component Setup

--- component_setup.sh
+++ component_setup.sh
@@ -11,34 +11,34 @@ func_exit_status() {
 
 
 func_apppreq(){
-    echo -e "\e[32mCreating the ${component} application service file.\e[0m" | tee -a $log
+    echo -e "\e[36mCreating the ${component} application service file.\e[0m" | tee -a $log
     cp service-files/${component}.service /etc/systemd/system/${component}.service &>> $log
     func_exit_status
 
-    echo -e "\e[32mCreating the application user.\e[0m" | tee -a $log
+    echo -e "\e[36mCreating the application user.\e[0m" | tee -a $log
     useradd roboshop -c "Application User" &>> $log
     func_exit_status
 
-    echo -e "\e[32mRemoving the old ${component} application directory.\e[0m" | tee -a $log
+    echo -e "\e[36mRemoving the old ${component} application directory.\e[0m" | tee -a $log
     rm -rf /app &>> $log
     func_exit_status
 
-    echo -e "\e[32mCreating the ${component} application directory.\e[0m" | tee -a $log
+    echo -e "\e[36mCreating the ${component} application directory.\e[0m" | tee -a $log
     mkdir /app &>> $log
     func_exit_status
 
-    echo -e "\e[32mDownloading the ${component} application content code.\e[0m" | tee -a $log
+    echo -e "\e[36mDownloading the ${component} application content code.\e[0m" | tee -a $log
     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> $log
     func_exit_status
 
-    echo -e "\e[32mExtracting the ${component} application code.\e[0m" | tee -a $log
+    echo -e "\e[36mExtracting the ${component} application code.\e[0m" | tee -a $log
     unzip -o /tmp/${component}.zip -d /app &>> $log
     func_exit_status
 }
 
 
 func_systemd(){
-    echo -e "\e[32mEnabling and restarting the ${component} application service.\e[0m" | tee -a $log
+    echo -e "\e[36mEnabling and restarting the ${component} application service.\e[0m" | tee -a $log
     systemctl daemon-reload &>> $log
     systemctl enable ${component}.service &>> $log
     systemctl restart ${component}.service &>> $log
@@ -48,25 +48,25 @@ func_systemd(){
 
 func_schema_setup(){
   if [ "${schema_type}" = "mongodb" ]; then
-    echo -e "\e[32mConfiguring the MongoDB repository.\e[0m" | tee -a $log
+    echo -e "\e[36mConfiguring the MongoDB repository.\e[0m" | tee -a $log
     cp repository-files/mongodb-org-4.2.repo /etc/yum.repos.d/mongodb-org-4.2.repo &>> $log
     func_exit_status
 
-    echo -e "\e[32mInstalling MongoDB client to load the schema.\e[0m" | tee -a $log
+    echo -e "\e[36mInstalling MongoDB client to load the schema.\e[0m" | tee -a $log
     dnf install mongodb-org-shell -y &>> $log
     func_exit_status
 
-    echo -e "\e[32mLoading the schema for the ${component} application.\e[0m" | tee -a $log
+    echo -e "\e[36mLoading the schema for the ${component} application.\e[0m" | tee -a $log
     mongo --host mongodb.learntechnology.cloud </app/schema/${component}.js &>> $log
     func_exit_status
   fi
 
   if [ "${schema_type}" = "mysql" ]; then
-    echo -e "\e[32mInstalling MySQL.\e[0m" | tee -a $log
+    echo -e "\e[36mInstalling MySQL.\e[0m" | tee -a $log
     dnf install mysql -y &>> $log
     func_exit_status
 
-    echo -e "\e[32mLoading the schema into MySQL.\e[0m" | tee -a $log
+    echo -e "\e[36mLoading the schema into MySQL.\e[0m" | tee -a $log
     mysql -h mysql.learntechnology.cloud -uroot -p${mysql_root_password} < /app/schema/${component}.sql &>> $log
     func_exit_status
   fi
@@ -74,24 +74,24 @@ func_schema_setup(){
 
 
 func_nodejs(){
-  echo -e "\e[1;36m--- ${component} Application Setup ---\e[0m" | tee -a $log
+  echo -e "\e[1;32m--- ${component} Application Setup ---\e[0m" | tee -a $log
 
-  echo -e "\e[32mDisabling the current Node.js module.\e[0m" | tee -a $log
+  echo -e "\e[36mDisabling the current Node.js module.\e[0m" | tee -a $log
   dnf module disable nodejs -y &>> $log
   func_exit_status
 
-  echo -e "\e[32mEnabling the Node.js 18 module.\e[0m" | tee -a $log
+  echo -e "\e[36mEnabling the Node.js 18 module.\e[0m" | tee -a $log
   dnf module enable nodejs:18 -y &>> $log
   func_exit_status
 
-  echo -e "\e[32mInstalling Node.js.\e[0m" | tee -a $log
+  echo -e "\e[36mInstalling Node.js.\e[0m" | tee -a $log
   dnf install nodejs -y &>> $log
   func_exit_status
 
   # Calling func_apppreq function
   func_apppreq
 
-  echo -e "\e[32mDownloading application dependencies from package.json.\e[0m" | tee -a $log
+  echo -e "\e[36mDownloading application dependencies from package.json.\e[0m" | tee -a $log
   npm install --prefix /app &>> $log
   func_exit_status
 
@@ -104,20 +104,20 @@ func_nodejs(){
 
 
 func_java(){
-  echo -e "\e[1;36m--- ${component} Application Setup ---\e[0m" | tee -a $log
+  echo -e "\e[1;32m--- ${component} Application Setup ---\e[0m" | tee -a $log
 
-  echo -e "\e[32mInstalling Maven.\e[0m" | tee -a $log
+  echo -e "\e[36mInstalling Maven.\e[0m" | tee -a $log
   dnf install maven -y &>> $log
   func_exit_status
 
   # Calling func_apppreq function
   func_apppreq
 
-  echo -e "\e[32mBuilding the application using Maven.\e[0m" | tee -a $log
+  echo -e "\e[36mBuilding the application using Maven.\e[0m" | tee -a $log
   mvn clean package -f /app/pom.xml &>> $log
   func_exit_status
 
-  echo -e "\e[32mMoving the application artifact file to /app.\e[0m" | tee -a $log
+  echo -e "\e[36mMoving the application artifact file to /app.\e[0m" | tee -a $log
   mv /app/target/${component}-1.0.jar /app/${component}.jar &>> $log
   func_exit_status
 
@@ -129,9 +129,9 @@ func_java(){
 
 
 func_python() {
-  echo -e "\e[1;36m--- ${component} Application Setup ---\e[0m" | tee -a $log
+  echo -e "\e[1;32m--- ${component} Application Setup ---\e[0m" | tee -a $log
 
-  echo -e "\e[32mInstalling Python 3.6, GCC (Compiler), and Python Development Build Tools.\e[0m" | tee -a $log
+  echo -e "\e[36mInstalling Python 3.6, GCC (Compiler), and Python Development Build Tools.\e[0m" | tee -a $log
   dnf install python36 gcc python3-devel -y &>> $log
   func_exit_status
 
@@ -141,7 +141,7 @@ func_python() {
   sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
   func_exit_status
 
-  echo -e "\e[32mInstalling the required Python dependencies.\e[0m" | tee -a $log
+  echo -e "\e[36mInstalling the required Python dependencies.\e[0m" | tee -a $log
   pip3.6 install -r /app/requirements.txt &>> $log
   func_exit_status
 
