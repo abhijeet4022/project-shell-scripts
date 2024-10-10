f71d7dcfae596f720f5053bda74a35c1de62a4ca
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-10 06:28:12 +0530

    Project: Payment Component

--- 10-payment.sh
+++ 10-payment.sh
@@ -3,7 +3,7 @@ source ./component_setup.sh
 
 rabbitmq_app_password=$1
 if [ -z "${rabbitmq_app_password}" ]; then
-  echo "Input RabbitMQ AppUser Password Missing"
+  echo "Input RabbitMQ AppUser Password Missing: Please pass the rabbitmq user password."
   exit 1
 fi
 
