51beac63d2a17895b62a73ea12e0582b4809cea3
abhijeet4022 <abhijeet4022@gmail.com>
2024-10-10 06:28:30 +0530

    Project: Component Setup

--- component_setup.sh
+++ component_setup.sh
@@ -5,7 +5,7 @@ func_exit_status() {
   if [ $? -eq 0 ]; then
     echo -e "\e[32m SUCCESS \e[0m"
   else
-    echo -e "\e[31m FAILURE \e[0m"
+    echo -e "\e[31m FAILURE: Please see the logs on /tmp/${component}.log \e[0m"
   fi
 }
 
